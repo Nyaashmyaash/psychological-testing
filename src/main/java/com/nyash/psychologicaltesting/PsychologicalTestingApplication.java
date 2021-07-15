494f85170ba395b2acc3ccb101a7b0a568842381
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-16 03:22:45 +0700

    Initial commit

---
+++ src/main/java/com/nyash/psychologicaltesting/PsychologicalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.psychologicaltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalTestingApplication.class, args);
+    }
+
+}
