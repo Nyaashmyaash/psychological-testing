2f5aa988246e91350444d3e0943a0abdc3deaff8
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-16 09:31:24 +0700

    add changes

---
+++ src/main/java/com/nyash/psychologicaltesting/api/store/entities/AnswerEntity.java
@@ -0,0 +1,27 @@
+package com.nyash.psychologicaltesting.api.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "answer")
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "answer_order")
+    private Integer answerOrder;
+
+    @Column(length = 10485760)
+    private String name;
+}
