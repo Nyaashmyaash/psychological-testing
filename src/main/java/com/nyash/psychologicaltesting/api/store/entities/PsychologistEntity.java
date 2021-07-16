5a86b38c22a3de15730712e2373ab8ae5688b004
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-16 09:35:09 +0700

    add changes

---
+++ src/main/java/com/nyash/psychologicaltesting/api/store/entities/PsychologistEntity.java
@@ -0,0 +1,33 @@
+package com.nyash.psychologicaltesting.api.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "psychologist")
+public class PsychologistEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+}
