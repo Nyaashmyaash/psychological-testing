d8cfb10751bbb3775b9c27f4c860d3cb1f46d406
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-17 01:24:16 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/store/entities/TestUserEntity.java
+++ src/main/java/com/nyash/psychologicaltesting/api/store/entities/TestUserEntity.java
@@ -1,4 +1,39 @@
 package com.nyash.psychologicaltesting.api.store.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "test_user")
 public class TestUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NonNull
+    @ManyToOne
+    private UserEntity user;
+
+    @NonNull
+    @ManyToOne
+    private TestEntity test;
+
+    @Column(length = 10485760)
+    @NonNull
+    private String answers;
+
+    @Builder.Default
+    @NonNull
+    @Column(name = "create_at")
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "psychologist_id", insertable = false, updatable = false)
+    private Long psychologistId;
 }
