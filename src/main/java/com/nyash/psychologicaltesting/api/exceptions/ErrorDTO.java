0f5e8176013e8e7195b0006d873c5923a1391875
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-16 08:40:34 +0700

    add changes

---
+++ src/main/java/com/nyash/psychologicaltesting/api/exceptions/ErrorDTO.java
@@ -0,0 +1,20 @@
+package com.nyash.psychologicaltesting.api.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+
+    private String error;
+
+    @JsonProperty("error_description")
+    private String errorDescription;
+}
