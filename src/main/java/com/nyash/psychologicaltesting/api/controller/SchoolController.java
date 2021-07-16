34b7f54b10314ae78647f978364d6a29c5f2cab4
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-17 04:41:11 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/controller/SchoolController.java
+++ src/main/java/com/nyash/psychologicaltesting/api/controller/SchoolController.java
@@ -1,6 +1,8 @@
 package com.nyash.psychologicaltesting.api.controller;
 
+import com.nyash.psychologicaltesting.api.dto.SchoolDTO;
 import com.nyash.psychologicaltesting.api.exceptions.BadRequestException;
+import com.nyash.psychologicaltesting.api.factory.SchoolDTOFactory;
 import com.nyash.psychologicaltesting.api.store.entities.SchoolEntity;
 import com.nyash.psychologicaltesting.api.store.repositories.SchoolRepository;
 import lombok.AccessLevel;
@@ -9,8 +11,12 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -20,10 +26,14 @@ public class SchoolController {
 
     SchoolRepository schoolRepository;
 
+    SchoolDTOFactory schoolDTOFactory;
+
     private static final String CREATE_SCHOOL = "/api/school/{schoolName}";
 
+    private static final String FETCH_SCHOOL = "/api/school";
+
     @PostMapping(CREATE_SCHOOL)
-    public ResponseEntity<?> createSchool(@PathVariable String schoolName) {
+    public ResponseEntity<SchoolDTO> createSchool(@PathVariable String schoolName) {
 
         if (schoolRepository.existsByName(schoolName)) {
             throw new BadRequestException
@@ -33,6 +43,14 @@ public class SchoolController {
         SchoolEntity school = schoolRepository.saveAndFlush(
                 SchoolEntity.makeDefault(schoolName));
 
-        return null;
+        return ResponseEntity.ok(schoolDTOFactory.createSchoolDTO(school));
+    }
+
+    @GetMapping(FETCH_SCHOOL)
+    public ResponseEntity<List<SchoolDTO>> fetchSchools(@RequestParam String filter) {
+
+            boolean isFiltered = !filter.trim().isEmpty();
+
+            List<SchoolEntity> schools = schoolRepository.
     }
 }
--- src/main/java/com/nyash/psychologicaltesting/api/store/repositories/SchoolRepository.java
+++ src/main/java/com/nyash/psychologicaltesting/api/store/repositories/SchoolRepository.java
@@ -3,8 +3,15 @@ package com.nyash.psychologicaltesting.api.store.repositories;
 import com.nyash.psychologicaltesting.api.store.entities.SchoolEntity;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SchoolRepository extends JpaRepository<SchoolEntity, Long> {
 
     Boolean existsByName(@NonNull String name);
+
+    @Query("SELECT s FROM SchoolEntity s" +
+            "WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :filter, '%'))")
+    List<SchoolEntity> findAllByFilter(String filter);
 }
