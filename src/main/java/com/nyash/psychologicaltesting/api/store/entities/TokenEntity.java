6a2d05505054c82479b542bd366358535b398626
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-17 01:41:57 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/store/entities/TokenEntity.java
+++ src/main/java/com/nyash/psychologicaltesting/api/store/entities/TokenEntity.java
@@ -1,4 +1,31 @@
 package com.nyash.psychologicaltesting.api.store.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
 public class TokenEntity {
+
+    private static final int EXPIRED_SECONDS = 60 * 60; // One hour expired
+
+    @Id
+    @Builder.Default
+    private String token = UUID.randomUUID().toString();
+
+    @Builder.Default
+    private Instant expiredAt = Instant.now().plusSeconds(EXPIRED_SECONDS);
+
+    @Builder.Default
+    private Instant createdAt = Instant.now();
 }
