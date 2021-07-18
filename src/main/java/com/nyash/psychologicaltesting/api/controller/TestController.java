e7d6c03d15534dde5bb425e142c406427e8dd481
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-19 03:27:03 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/controller/TestController.java
+++ src/main/java/com/nyash/psychologicaltesting/api/controller/TestController.java
@@ -9,6 +9,7 @@ import com.nyash.psychologicaltesting.api.factory.UserDTOFactory;
 import com.nyash.psychologicaltesting.api.store.entities.SchoolClassEntity;
 import com.nyash.psychologicaltesting.api.store.entities.UserEntity;
 import com.nyash.psychologicaltesting.api.store.repositories.SchoolClassRepository;
+import com.nyash.psychologicaltesting.api.store.repositories.TestRepository;
 import com.nyash.psychologicaltesting.api.store.repositories.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -27,81 +28,13 @@ import java.util.List;
 @Transactional
 public class TestController {
 
-    UserDTOFactory userDTOFactory;
+    TestRepository testRepository;
 
-    UserRepository userRepository;
-
-    SchoolClassRepository schoolClassRepository;
-
-    public static final String FETCH_USERS = "/api/schools/classes/users";
-    public static final String CREATE_USER = "/api/schools/classes/{classId}/users";
-    public static final String DELETE_USER = "/api/schools/classes/users/{userId}";
+    public static final String FETCH_TESTS = "/api/tests";
+    public static final String CREATE_OR_UPDATE_TEST = "/api/tests";
+    public static final String DELETE_TEST = "/api/tests/{testId}";
 
 
-    @PostMapping(CREATE_USER)
-    public ResponseEntity<SchoolDTO> createUser(
-            @RequestParam Instant birthday,
-            @RequestParam String firstName,
-            @RequestParam(defaultValue = "") String middleName,
-            @RequestParam String lastName,
-            @RequestParam UserRole userRole,
-            @PathVariable Long classId) {
 
-        firstName = firstName.trim();
-        lastName = lastName.trim();
-        middleName = middleName.trim().isEmpty() ? null : middleName;
-
-        String login = makeLogin(firstName,lastName);
-        String password = makePassword();
-
-        SchoolClassEntity schoolClass = schoolClassRepository
-                .findById(classId)
-                .orElseThrow(() ->
-                        new NotFoundException(String.format("Class with id \"%s\" cannot found", classId)));
-
-        UserEntity user = userRepository.saveAndFlush(
-                UserEntity.makeDefault(
-                        firstName,
-                        middleName,
-                        lastName,
-                        login,
-                        password,
-                        birthday,
-                        userRole,
-                        schoolClass
-                )
-        );
-
-        return ResponseEntity.ok(userDTOFactory.createUserDTO(user))
-    }
-
-    @GetMapping(FETCH_USERS)
-    public ResponseEntity<List<UserDTO>> fetchUsers(@RequestParam String filter) {
-
-            boolean isFiltered = !filter.trim().isEmpty();
-
-            List<UserEntity> users = userRepository.findAllByFilter(isFiltered, filter);
-
-            return ResponseEntity.ok(UserDTOFactory.createUserDTOList(users));
-    }
-
-    @DeleteMapping(DELETE_USER)
-    public ResponseEntity<AckDTO> deleteUser(@PathVariable Long userId) {
-
-        if (userRepository.existsById(userId)) {
-            userRepository.deleteById(userId);
-        }
-
-        return ResponseEntity.ok(AckDTO.makeDefault(true));
-
-    }
-
-    private String makeLogin(){
-
-    }
-
-    private String makePassword() {
-
-    }
 
 }
