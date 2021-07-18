a6db4bf496de8cb4656ef82be83abb2d68a3ece8
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-19 03:40:40 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/controller/TestController.java
+++ src/main/java/com/nyash/psychologicaltesting/api/controller/TestController.java
@@ -1,26 +1,19 @@
 package com.nyash.psychologicaltesting.api.controller;
 
-import com.nyash.psychologicaltesting.api.domains.UserRole;
-import com.nyash.psychologicaltesting.api.dto.AckDTO;
-import com.nyash.psychologicaltesting.api.dto.SchoolDTO;
-import com.nyash.psychologicaltesting.api.dto.UserDTO;
 import com.nyash.psychologicaltesting.api.exceptions.NotFoundException;
-import com.nyash.psychologicaltesting.api.factory.UserDTOFactory;
-import com.nyash.psychologicaltesting.api.store.entities.SchoolClassEntity;
-import com.nyash.psychologicaltesting.api.store.entities.UserEntity;
-import com.nyash.psychologicaltesting.api.store.repositories.SchoolClassRepository;
+import com.nyash.psychologicaltesting.api.store.entities.TestEntity;
 import com.nyash.psychologicaltesting.api.store.repositories.TestRepository;
-import com.nyash.psychologicaltesting.api.store.repositories.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.Instant;
-import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -34,7 +27,29 @@ public class TestController {
     public static final String CREATE_OR_UPDATE_TEST = "/api/tests";
     public static final String DELETE_TEST = "/api/tests/{testId}";
 
+//    @GetMapping(FETCH_TESTS)
+//    public ResponseEntity<?> fetchTests() {
+//
+//    }
 
+    @PostMapping(CREATE_OR_UPDATE_TEST)
+    public ResponseEntity<?> createOrUpdateTest(
+            @RequestParam(required = false) Long testId,
+            @RequestParam(required = false) Long psychologistId,
+            @RequestParam(required = false) String testName) {
 
+        TestEntity test;
 
+        if (testId != null) {
+            test = testRepository
+                    .findById(testId)
+                    .orElseThrow(() ->
+                            new NotFoundException(String.format("Тест с идентификатором \"%s\" не найден.", testId))
+                    );
+        }
+
+        Optional.ofNullable(testId)
+                .map(testRepository::findById)
+                .or
+    }
 }
