84f5bf46c820db2dc6d7187e3ed8a30cfa86bf52
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-20 21:14:28 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/factory/TestUserDTOFactory.java
+++ src/main/java/com/nyash/psychologicaltesting/api/factory/TestUserDTOFactory.java
@@ -1,4 +1,30 @@
 package com.nyash.psychologicaltesting.api.factory;
 
+import com.nyash.psychologicaltesting.api.dto.TestUserDTO;
+import com.nyash.psychologicaltesting.api.store.entities.TestUserEntity;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Component
 public class TestUserDTOFactory {
+
+    TestDTOFactory testDTOFactory;
+
+    UserDTOFactory userDTOFactory;
+
+    public List<TestUserDTO> createTestUserDTOList(List<TestUserEntity> entities) {
+
+        return entities.stream().map()
+    }
+
+    public TestUserDTO createTestUserDTO(TestUserEntity entity) {
+        return TestUserDTO.builder()
+                .test()
+    }
 }
