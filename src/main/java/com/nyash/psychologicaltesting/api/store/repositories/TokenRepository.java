37cd227a62caec163ea334cd0425555da8309754
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-23 11:32:52 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/store/repositories/TokenRepository.java
+++ src/main/java/com/nyash/psychologicaltesting/api/store/repositories/TokenRepository.java
@@ -3,5 +3,8 @@ package com.nyash.psychologicaltesting.api.store.repositories;
 import com.nyash.psychologicaltesting.api.store.entities.TokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+import java.time.Instant;
+
+public interface TokenRepository extends JpaRepository<TokenEntity, String> {
+    void deleteAllByExpiredAtBefore(Instant currentDate);
 }
