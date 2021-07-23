6b300d11289f50523d7652d883cfaa838f566427
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-23 11:16:17 +0700

    add changes

--- src/main/java/com/nyash/psychologicaltesting/api/controller/AuthorizationController.java
+++ src/main/java/com/nyash/psychologicaltesting/api/controller/AuthorizationController.java
@@ -5,7 +5,10 @@ import com.nyash.psychologicaltesting.api.store.repositories.TokenRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
 
@@ -20,4 +23,13 @@ public class AuthorizationController {
     PsychologistRepository psychologistRepository;
 
     public static final String AUTHORIZE = "/api/psychologists/authorizations";
+
+    @GetMapping(AUTHORIZE)
+    public ResponseEntity<String> authorize(
+            @RequestParam String login,
+            @RequestParam String password) {
+
+        psychologistRepository
+                .findTop
+    }
 }
--- src/main/java/com/nyash/psychologicaltesting/api/store/repositories/PsychologistRepository.java
+++ src/main/java/com/nyash/psychologicaltesting/api/store/repositories/PsychologistRepository.java
@@ -1,7 +1,11 @@
 package com.nyash.psychologicaltesting.api.store.repositories;
 
 import com.nyash.psychologicaltesting.api.store.entities.PsychologistEntity;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PsychologistRepository extends JpaRepository<PsychologistEntity, Long> {
+    Optional<PsychologistEntity> findTopByLoginAndPassword(@NonNull String login, @NonNull String password);
 }
