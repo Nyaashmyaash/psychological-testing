eeb111a454841ffd27b1c5f2f92064924e4c8e89
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-29 00:52:02 +0700

    add changes in auth service

--- src/main/java/com/nyash/psychologicaltesting/api/service/ControllerAuthenticationService.java
+++ src/main/java/com/nyash/psychologicaltesting/api/service/ControllerAuthenticationService.java
@@ -1,4 +1,25 @@
 package com.nyash.psychologicaltesting.api.service;
 
+import com.nyash.psychologicaltesting.api.exceptions.UnauthorizedException;
+import com.nyash.psychologicaltesting.api.store.repositories.TokenRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Controller
+@Transactional
 public class ControllerAuthenticationService {
+
+    TokenRepository tokenRepository;
+
+    public void authenticate(String token) {
+
+        tokenRepository.findById(token)
+                .orElseThrow(() -> new UnauthorizedException("Для выполнения этого действия необходимо войти в систему")
+                );
+    }
 }
